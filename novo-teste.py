07eaffa9498f8b2117f85e1c3783f94d50f044f6
robertin <robertmaklyns@gmail.com>
2023-10-13 10:14:05 -0300

    novo teste

--- novo-teste.py
+++ novo-teste.py
@@ -8,12 +8,20 @@ class Pizza:
         if Pizza.pedacos > 0:
             Pizza.pedacos -= 1
         else:
-            print("Acabou a pizza")
+            print("acabou a pizza")
 
     @classmethod
-    def mudar_tamanho(cls, pedacos):
-        cls.pedacos = pedacos  # Modificar o atributo de classe pedacos
 
+    def mudar_tamanho(cls, pedacos):
+        cls.pedacos = pedacos
+
+p = Pizza()
+print(Pizza.pedacos)
+p.pegar_pedaco()
+p.pegar_pedaco()
 print(Pizza.pedacos)
 Pizza.mudar_tamanho(12)
 print(Pizza.pedacos)
+p.pegar_pedaco()
+p.pegar_pedaco()
+print(p.pedacos)
